b4641b6a5d99c2860c21680c1d4437cae524c924
Sasha <F712@rambler.ru>
2022-10-13 15:24:03 +0300

    Задача

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string[] array1 = {"Hello", "2", "world", ":-)"};
+string[] array2 = new string[array1.Length]; // массивы по длинне одинаковые (не оч)???
+PrintArray(array1);
+SecondArray(array1, array2);
+PrintArray(array2);
+
+void SecondArray(string[]array1, string[]array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+    if (count==0) 
+        {
+        Console.WriteLine("Нет элементов");
+        }
+}
+void PrintArray(string[]array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}  ");
+    }
+    Console.WriteLine();
+}
